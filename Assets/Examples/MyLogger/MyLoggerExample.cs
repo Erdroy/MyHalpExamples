d0c02d91873e4b449ad139dbb5f7e3400c50a1ed
Damian Korczowski <erdroy2@gmail.com>
2017-03-31 13:51:34 +0200

    initial commit

---
+++ Assets/Examples/MyLogger/MyLoggerExample.cs
@@ -0,0 +1,56 @@
+﻿using MyHalp;
+using UnityEngine;
+
+public class MyLoggerExample : MyComponent
+{
+    protected override void OnInit()
+    {
+        // this is optional
+        MySettings.UseDispatchedLogCallback = false;
+        MySettings.UseLogCallback = true;
+        MySettings.ProduceLogFile = true;
+        MySettings.LogFile = "log.txt";
+
+        MyLogger.Add("Hello, World!");
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("MyLogger.Add()"))
+        {
+            MyLogger.Add("New log!");
+        }
+
+        if (GUILayout.Button("Debug.Log()"))
+        {
+            Debug.Log("New log using Debug.Log()!");
+        }
+
+        if (GUILayout.Button("Threaded MyLogger.Add()"))
+        {
+            MyJob.Run(delegate
+            {
+                MyLogger.Add("Threaded new log!");
+            });
+        }
+
+        if (GUILayout.Button("Threaded Debug.Log()"))
+        {
+            MyJob.Run(delegate
+            {
+                Debug.Log("Threaded new log using Debug.Log()!");
+            });
+        }
+        if (GUILayout.Button("Point"))
+        {
+            MyLogger.Point(); // show point
+        }
+        if (GUILayout.Button("Threaded Point"))
+        {
+            MyJob.Run(delegate
+            {
+                MyLogger.Point(); // show point
+            });
+        }
+    }
+}
---
+++ Assets/Examples/MyThreading/MyThreadingExample.cs
@@ -0,0 +1,101 @@
+﻿using MyHalp;
+using UnityEngine;
+
+public class MyThreadingExample : MyComponent
+{
+    private bool _working;
+    private bool _locked;
+    private MyJobQueue _queue;
+
+    protected override void OnInit()
+    {
+        // initialize dispatcher
+        MyDispatcher.Init();
+    }
+
+    private void OnGUI()
+    {
+        if (_working)
+        {
+            GUILayout.Label("Progress: " + (_queue.GetProgress() * 100.0f).ToString("f1") + "%");
+        }
+
+        if (GUILayout.Button("Start Simple")) // SIMPLE
+        {
+            StartSimple();
+        }
+
+        if (GUILayout.Button("Start Advanced")) // ADVANCED
+        {
+            StartAdvanced();
+        }
+
+        if (GUILayout.Button("Unlock"))
+        {
+            _locked = false;
+        }
+    }
+
+    private void StartSimple()
+    {
+        // create new job queue
+        var queue = MyJobQueue.Create(delegate
+        {
+            // this is the 'on done', this is called at the very end of the queue when all jobs are done
+            Debug.Log("Queue done!");
+        });
+
+        queue.Queue(delegate
+        {
+            Debug.Log("Hello!");
+        });
+
+        // start executing the queue
+        queue.Execute();
+    }
+
+    private void StartAdvanced()
+    {
+        _working = true;
+        _locked = true;
+
+        // create new job queue
+        _queue = MyJobQueue.Create(delegate
+        {
+            _working = false;
+
+            // this is the 'on done', this is called at the very end of the queue when all jobs are done
+            Debug.Log("Queue done!");
+        });
+
+        _queue.Queue(delegate
+        {
+            // wait until user clicks UNLOCK button, use custom precision(16 ms, about one frame)
+            MyJob.Wait(ref _locked, 16);
+
+            Debug.Log("Working!");
+        });
+
+        // queue some more and 'heavy' jobs
+        for (var i = 0; i < 25; i++)
+        {
+            // queue new job
+            _queue.Queue(delegate
+            {
+                MyDispatcher.Dispatch(delegate
+                {
+                    // run this on the main-thread
+                    Debug.Log("Hello, from the main thread!");
+                });
+
+                // sleep for 0.5 seconds
+                MyJob.Wait(0.5f);
+            });
+        }
+
+        Debug.Log("Starting the execution!");
+
+        // start executing the queue
+        _queue.Execute();
+    }
+}
