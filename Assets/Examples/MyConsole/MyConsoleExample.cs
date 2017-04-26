5bde3a99583705bd1563b6491feee5d7e7629ea1
Damian Korczowski <erdroy2@gmail.com>
2017-04-26 13:36:29 +0200

    added MyConsole example

---
+++ Assets/Examples/MyConsole/MyConsoleExample.cs
@@ -0,0 +1,27 @@
+﻿using MyHalp;
+using UnityEngine;
+
+public class MyConsoleExample : MyComponent
+{
+    protected override void OnInit()
+    {
+        // init mylogger
+        MySettings.UseDispatchedLogCallback = false;
+        MySettings.UseLogCallback = true;
+
+        // init my console
+        MyConsole.Init();
+
+        // say something!
+        MyLogger.Add("Hello, World!");
+        MyLogger.Add("Ow, shit, error.", MyLoggerLevel.Error);
+    }
+
+    protected override void OnTick()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            MyLogger.Add("Some message", (MyLoggerLevel)Random.Range(0, 5));
+        }
+    }
+}
