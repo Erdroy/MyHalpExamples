f7c59b565c4bc2c608e9799fe0e5a88034eb3ea3
Damian Korczowski <erdroy2@gmail.com>
2017-04-27 12:44:21 +0200

    added some more example commands

--- Assets/Examples/MyConsole/MyConsoleExample.cs
+++ Assets/Examples/MyConsole/MyConsoleExample.cs
@@ -34,6 +34,36 @@ public class MyConsoleExample : MyComponent
             MyLogger.Add("Integer: " + integer);
         });
 
+        MyCommands.Instance.RegisterCommand("", "volume", (float level) =>
+        {
+            MyLogger.Add("Volume: " + level);
+        }, "Sets the overall game volume");
+
+        MyCommands.Instance.RegisterCommand("", "sfx_volume", (float level) =>
+        {
+            MyLogger.Add("Volume: " + level);
+        }, "Sets the sound effects volume");
+
+        MyCommands.Instance.RegisterCommand("", "vfx_volume", (float level) =>
+        {
+            MyLogger.Add("Volume: " + level);
+        }, "Sets the voice volume");
+
+        MyCommands.Instance.RegisterCommand("", "say", (string message) =>
+        {
+            MyLogger.Add("Says: " + message);
+        }, "Say something through the game chat");
+
+        MyCommands.Instance.RegisterCommand("", "spawn_entity", (string entityName) =>
+        {
+            MyLogger.Add("Spawned: " + entityName + " id: " + 100);
+        }, "Spawns entity by name");
+
+        MyCommands.Instance.RegisterCommand("", "destroy_entity", (int entId) =>
+        {
+            MyLogger.Add("Destroyed entity with id: " + entId);
+        }, "Destroys entity by entity id.");
+
         // params will be correct, no need to check
     }
 
